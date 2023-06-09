12635d2d6960235f0ad94f6fccfa98e019c051f9
BDG0DIRECTS <134042806+BDG0DIRECTS@users.noreply.github.com>
2023-06-09 09:30:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = "27222161"
+API_HASH     = "aa8a91b68826efcc54be159ea8a3217a"
+BOT_TOKEN    = "6053302009:AAE-phtvbCu6bbIq3qDiBcRyiIAOkSYRUGs"
+SESSION      = "BQGfYJEAhdK4W0SA2qpH1PVc1oDmtNjD50NNOxnffxnJGX1_SAXPEWmyK8uiB0dwXoPTGqEMq6Xs4vua6W5nHEUDeR39ALI1tWDhPEE6OFooYW_uAXj897cqaza5DB5ncBw-sYp6tNA2fETv56L2JaEg5_qwuYzqrT9UJbuPGdbSxx-rDnp0o7a4qhQvuhOMfdEQ0Tz7i4JNToaLLskrdsmwOmnZF2G8mYmX2fNrtdou4zVRBUH53CoriWiKmeJzxGqJbO8EePqiH5ThbB-tWJqrOXMyjWMkcL_eG6W-04f7WUTX22DzGxHqwmvnZpOUuJJ-8TX1U0eSM1x8BzLURJqazAbwAAAAB8RfcIAA"
+DATABASE_URI = "mongodb+srv://kaddayash:<password>@cluster0.p4vjbgn.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001955059837
+ADMIN        = 6022333221
+CHANNEL      = "@official_kadda"
